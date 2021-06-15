ac2c361fc37fa6291c0aa36b53e3cb53c419dcd0
KiwiRobbie <46206090+KiwiRobbie@users.noreply.github.com>
2021-06-16 11:22:06 +1200

    Started developing queue widget
    Started the development of a custom tkinter widget for holding
    scrollable queues of other widgets

--- Card.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-import random
-import math 
-
-# Class for creating question and infomation cards
-class Card():
-    def __init__(self,root,card_file):
-        pass
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from tkinter.constants import RIDGE
 from WindowDark import WindowDark
+import UI
+import time
+from math import sin
 
-root=WindowDark(500,700,0,0)
-root.window.mainloop()
+root=WindowDark(480,700,0,0)
+button=UI.Card(root.root,0)
+main_queue=UI.Queue(root.root)
+
+while True:
+    button.update(100)
+    root.window.update()
+
