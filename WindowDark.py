606e1d2664ee4a07e22336283c6e2bdb3170a91e
KiwiRobbie <r.b.christie@icloud.com>
2021-06-23 22:28:54 +1200

    Added stuff for scrolling and UI

--- WindowDark.py
+++ WindowDark.py
@@ -16,20 +16,21 @@ colour=DarkTheme()
 
 #WindowDark is an extension of the tkinter.Tk class
 import tkinter as tk
-class WindowDark():
+from tkinter import ttk
 
+class WindowDark(tk.Tk):
     #Override the defualt init for Tk window class
     def __init__(self, w, h, x=710,y=100):
         #Create a new window using overide dirrect
-        self.window=tk.Tk()
-        self.window.geometry('{}x{}+{}+{}'.format(w,h+20,int((1920-w)/2),int((1080-h-20)/2)))
-        self.window.overrideredirect(True)
-        self.window.config(bg=colour.bg_1)
+        super().__init__()
+        self.geometry('{}x{}+{}+{}'.format(w,h+20,int((1920-w)/2),int((1080-h-20)/2)))
+        self.overrideredirect(True)
+        self.config(bg=colour.bg_1)
         self.last_x=0
         self.last_y=0
 
         #Create a title bar
-        window_bar = tk.Frame(self.window,bg=colour.bg_1,width=500,height=20)
+        window_bar = tk.Frame(self,bg=colour.bg_1,width=500,height=20)
         window_bar.place(x=0,y=0)
         window_bar.lift()
 
@@ -46,17 +47,18 @@ class WindowDark():
         window_bar.bind('<B1-Motion>', self.move_window)
         window_bar.bind('<ButtonPress-1>', self.mouse_down)
 
-        self.root=tk.Frame(self.window,width=w,height=h,bg=colour.bg_3)
-        self.root.columnconfigure(0,weight=1)
-        self.root.rowconfigure(0,weight=1)
+        self.root=tk.Frame(self,width=w,height=h,bg=colour.bg_3)
+        self.columnconfigure(0,weight=1)
+        self.rowconfigure(0,weight=1)
         self.root.grid_propagate(False)
         self.root.place(x=0,y=20)
+        
 
     #Fuction to update the window when the mouse is moved
     def move_window(self,event):
         delta_x=event.x_root-self.last_x 
         delta_y=event.y_root-self.last_y 
-        self.window.geometry('+{0}+{1}'.format(self.window.winfo_x() + delta_x ,self.window.winfo_y()+delta_y))
+        self.geometry('+{0}+{1}'.format(self.winfo_x() + delta_x ,self.winfo_y()+delta_y))
         self.last_x=event.x_root
         self.last_y=event.y_root
 
@@ -67,7 +69,7 @@ class WindowDark():
 
     #Function to quit the program
     def quit(self):
-        self.window.destroy()
+        self.destroy()
 
 #Run the main script if we try to run the this file instead
 if(__name__=='__main__'):
