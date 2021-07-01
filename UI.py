ab2511717a47333b67f828e4f19d9709dd91f996
KiwiRobbie <46206090+KiwiRobbie@users.noreply.github.com>
2021-07-02 10:51:52 +1200

    Restructured cards to have dynamic layout
    Changes were made to the card class making their layout more dynamic,
    cards can now be created by combining several different ui elements in
    any order.

--- UI.py
+++ UI.py
@@ -3,61 +3,61 @@ from UI_Styles import DarkTheme
 
 colour = DarkTheme()
 
-
-class ConfigureCard:
-    def __init__(self) -> None:
-        pass
-
-
 class Card(tk.Frame):
-    def __init__(self, root, card_config) -> None:
+    def __init__(self, root) -> None:
         super().__init__(root, bg=colour.bg_3)
-        self.config = card_config
         self.columnconfigure(0, weight=1)
 
-        # Card title
-        self.title_frame = tk.Frame(self, width=320, height=16, bg=colour.bg_3)
+        self.title = 0
+        self.body = []
+        self.input = 0
+        self.responses = []
+        self.correct_response = []
+        self.content = 0
+
+    # Add a banner holding the title to the top of the card
+    def add_title(self,title):
+        self.title_frame = tk.Frame(self, width=360, height=24, bg=colour.bg_3)
         self.title_frame.grid(row=0, column=0, sticky='NSEW', pady=2)
         self.title_frame.columnconfigure(0, weight=1)
         self.title_frame.rowconfigure(0, weight=1)
         self.title_frame.grid_propagate(False)
- 
-        self.title = tk.Label(self.title_frame, text='Complex Numbers', fg=colour.txt_2, font='Corbel 12 bold', bd=0,
+
+        self.title = tk.Label(self.title_frame, text=title, fg=colour.txt_2, font='Corbel 12 bold', bd=0,
                               bg=colour.bg_2)
-        self.title.grid(row=0, column=0, sticky='NSEW')
+        self.title.grid(column=0, sticky='NSEW')
 
-        # Question
-        self.body = tk.Frame(self, width=320, height=100, bg=colour.bg_2)
-        self.body.grid(row=1, column=0, sticky='NSEW', pady=2)
+    def add_content(self):
+        self.content = tk.Frame(self, width=320, bg=colour.bg_2)
+        self.content.grid(column=0, sticky='NSEW', pady=2)
+        self.content.columnconfigure(0,weight=1)
 
-        # Button
-        if self.config.button == 'single':
-            self.single_button()
-        elif self.config.button == 'tri':
-            self.tri_button()
+    # Add a section of text to the card
+    def add_text(self,text,font='Corbel 11'):
+        self.body.append(tk.Label(self.content,text=text,bg=colour.bg_2,fg=colour.txt_1, font=font))
+        self.body[-1].grid(column=0, sticky='NSEW',pady=4)
 
-    def enter(self, event):
-        pass
+    # Add a single button to accept the card
+    def add_single_button(self):
+        self.input = tk.Button(self, text='Ok', font='Corbel 12', relief='flat', bg=colour.bg_2, fg=colour.txt_1)
+        self.input.grid(column=0, sticky='NSEW', pady=2)
 
-    def exit(self, event):
-        pass
+    # Add a multichoice input
+    def add_tri_button(self, answers, correct):
+        self.responses = answers
+        self.correct_response = correct
 
-    def single_button(self):
-        self.button = tk.Button(self, text='Ok', font='Corbel 12', relief='flat', bg=colour.bg_2, fg=colour.txt_1)
-        self.button.grid(row=2, column=0, sticky='NSEW', pady=2)
-
-    def tri_button(self):
-        self.button_frame = tk.Frame(self, width=320, height=32, bg=colour.bg_3)
-        self.button_frame.grid(row=2, column=0, sticky='NSEW', pady=2)
-        self.button_frame.rowconfigure(0, weight=1)
-        self.button_frame.grid_propagate(False)
+        self.input = tk.Frame(self, width=320, height=32, bg=colour.bg_3)
+        self.input.grid(column=0, sticky='NSEW', pady=2)
+        self.input.rowconfigure(0, weight=1)
+        self.input.grid_propagate(False)
 
         self.buttons = []
         for i in range(3):
-            self.button_frame.columnconfigure(i, weight=1)
+            self.input.columnconfigure(i, weight=1)
             self.buttons.append(
-                tk.Button(self.button_frame, text=self.config.answer[i], relief='flat', bd=0, bg=colour.bg_2,
-                          fg=colour.txt_1,command=self.correct if self.config.correct==i else self.incorrect))
+                tk.Button(self.input, text=self.responses[i], relief='flat', bd=0, bg=colour.bg_2,
+                          fg=colour.txt_1, command=self.correct if self.correct_response == i else self.incorrect))
             self.buttons[-1].grid(row=0, column=i, sticky='NESW', padx=(2 * (i != 0), 2 * (i != 2)))
 
     def correct(self):
@@ -91,8 +91,8 @@ class Queue:
         self.position += (self.target - self.position) * min(delta_t * 5.0, 1.0)
         self.root.place(x=60, y=self.position)
 
-    def append_card(self, card_config):
-        self.cards.append(Card(self.root, card_config))
+    def append_card(self,card):
+        self.cards.append(card)
         self.cards[-1].grid(row=len(self.cards) - 1, column=0, pady=15)
         self.root.update()
         self.length = max(600, self.root.winfo_height())
--- main.py
+++ main.py
@@ -13,11 +13,16 @@ last_t = time.time()
 delta_t = 0
 
 for i in range(10):
-    config = UI.ConfigureCard()
-    config.button = 'tri'
-    config.answer = ['A', 'B', 'C']
-    config.correct = random.randint(0, 2)
-    main_queue.append_card(config)
+    new_card=UI.Card(main_queue.root)
+    new_card.add_title('Complex Numbers')
+    new_card.add_content()
+    new_card.add_text('The answer is probably not B\n( Statistically speaking )')
+    new_card.add_text('I am not responsible for any issues caused if the answer is actually B',font='Corbel 7 italic')
+
+
+    new_card.add_tri_button(['A', 'B', 'C'], random.randint(0, 2))
+    main_queue.append_card(new_card)
+
 
 print('Test')
 
